e8902efd543f400a5700c0aa3a0a01329da6e376
Habil Yadigar <46094018+habilyadigar@users.noreply.github.com>
2021-01-10 02:59:24 +0300

    database changed and added safe connection

--- config/database.js
+++ config/database.js
@@ -2,24 +2,15 @@ const { createPool } = require("mysql")
 require('dotenv/config')
 
 const pool = createPool({
-    host: "mtsk.cdyumndry0mt.us-east-2.rds.amazonaws.com",
-    port: 3306,
-    user: "admin",
-    password: "admin159",
-    database: "MTSK2",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     connectionLimit:10 
 });
 
 
-
-//const pool = createPool({
-//    host: process.env.DB_HOST,
-//    port: process.env.DB_PORT,
-//    user: process.env.DB_USER,
-//    password: process.env.DB_PASSWORD,
-//    database: process.env.MYSQL_DB,
-//    connectionLimit:10 
-//});
 
 
 
