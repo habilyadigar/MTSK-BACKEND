5bd23f312cc82f0b63d386df92fe0fe5a14c319a
Habil Yadigar <46094018+habilyadigar@users.noreply.github.com>
2021-01-18 16:33:12 +0300

    env

--- config/database.js
+++ config/database.js
@@ -1,24 +1,14 @@
 const { createPool } = require("mysql")
 require('dotenv/config')
 
+
 const pool = createPool({
-    host: "mtsk-proje.c9qf5frog2vn.eu-central-1.rds.amazonaws.com",
-    port: 3306,
-    user: "habil",
-    password: "59aws991",
-    database: "MTSK",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     connectionLimit:10 
 });
-
-
-
-//const pool = createPool({
-//    host: process.env.DB_HOST,
-//    port: process.env.DB_PORT,
-//    user: process.env.DB_USER,
-//    password: process.env.DB_PASSWORD,
-//    database: process.env.DB_NAME,
-//    connectionLimit:10 
-//});
 
 module.exports = pool;
